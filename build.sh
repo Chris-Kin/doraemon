14841f977221ccc7cef661ecd3553cd8dd960b5d
于明严 <yumingyan@bytedance.com>
2024-06-18 17:07:57 +0800

    feat: build

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 版本打印
+node -v
+npm -v
+set -e
+. /home/tiger/.nvm/nvm.sh # 加载 SCM 机器上的 nvm 脚本
+nvm install v20
+nvm use v20
+
+echo 'use pnpm'
+npm i -g pnpm
+pnpm -v
+node -v
+npm -v
+
+export PERFSEE_TOKEN=uBO5buq//vB3/xXw8EF95jQXmzSvTVDzX1pqxs/p0Ddk=
+
+
+# FE Build
+pnpm install --no-frozen-lockfile
+
+pnpm run deploy
+mkdir -p ./output/resource/
+cp -rf ./output_resource/resource/* ./output/resource/
+
+env
