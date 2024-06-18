7f7fd8d1410a4f94cc7d818b5c3f8ae61f208cb3
yy <your.email@example.com>
2024-06-18 09:20:54 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Button } from '@/components/ui/button'
 
 defineProps<{ msg: string }>()
 
@@ -18,15 +19,7 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
+    <Button>Click me</Button>
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
